bae3988dd6d4b6e3e0d277ee6eedc1e7d5714653
Rakhila2005 <146660572+Rakhila2005@users.noreply.github.com>
2024-03-03 15:22:57 +0600

    Add files via upload

--- services/contact/cmd/app/main.go
+++ services/contact/cmd/app/main.go
@@ -1,1 +1,21 @@
+package main
 
+import (
+    "fmt"
+    "day2/pkg/store/postgres"
+)
+
+func main() {
+    host := "localhost"
+    port := "5432"
+    user := "postgres"
+    password := "1908"
+    dbname := "ass_3_go"
+
+    db, err := postgres.ConnectDB(host, port, user, password, dbname)
+    if err != nil {
+        fmt.Println("Ошибка подключения к базе данных:", err)
+        return
+    }
+    defer db.Close()
+}
