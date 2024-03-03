8d0f41b8048c1ec9062b2807c0dbc7255e6b1876
Rakhila2005 <146660572+Rakhila2005@users.noreply.github.com>
2024-03-03 15:16:20 +0600

    Add files via upload

--- pkg/store/postgres/postgres.go
+++ pkg/store/postgres/postgres.go
@@ -1,1 +1,25 @@
+package postgres
 
+import (
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
+)
+
+func ConnectDB(host, port, user, password, dbname string) (*sql.DB, error) {
+    connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        host, port, user, password, dbname)
+
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        return nil, err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    return db, nil
+}
